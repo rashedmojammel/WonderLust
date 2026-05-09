4f8808ca4e69c6a780eb11b634dc99e1e9905d84
rashedmojammel <rashedmojamme56@gmail.com>
2026-05-09 23:21:19 +0600

    feat : added db connection with server and client

--- Wanderlus-server/index.js
+++ Wanderlus-server/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const cors = require("cors")
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 dotenv.config();
 const app = express()
 const uri = process.env.MONGO_URI;
+app.use(cors())
+app.use(express.json())
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -16,14 +19,24 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
+     await client.connect();
+
+    const db = client.db("wonderlust")
+    const destinationCollection = db.collection("destinations");
+
+    app.post('/destination',(req,res) => {
+      const destinationData = req.body
+      const result = destinationCollection.insertOne(destinationData)
+      res.json(result)
+    })
+
+   
+   
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
